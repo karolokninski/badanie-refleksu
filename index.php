6a541cdcff11031bcfaf7cb6c3949ba288d7ac1a
kepucino <okninskikarol2@gmail.com>
2024-04-17 19:58:15 +0200

    Add index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Badanie refleksu</title>
+</head>
+
+<body>
+<h1>Test czasu reakcji</h1>
+<p>Zobaczysz pięć zgaszonych żarówek. Losowo jedna z nich się zaświeci. Twoim zadaniem będzie kliknąć na świecącą się żarówkę. Twój wynik pojawi się zaraz po zakończeniu testu.</p>
+</body>
+
+</html>
