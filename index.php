a599e1d056620f9df6f5602860faba563ee16161
kepucino <okninskikarol2@gmail.com>
2024-04-18 20:25:47 +0200

    Add assets

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
 <body>
 <h1>Test czasu reakcji</h1>
 <p>Zobaczysz pięć zgaszonych żarówek. Losowo jedna z nich się zaświeci. Twoim zadaniem będzie kliknąć na świecącą się żarówkę. Twój wynik pojawi się zaraz po zakończeniu testu.</p>
+<img src="assets/Lightbulb-Off.svg" style="width: 100px; height: 100px;">
+<img src="assets/Lightbulb-On.svg" style="width: 100px; height: 100px;">
 </body>
 
 </html>
